78e18fbcb29fe979ffe972aff6039547b9f337ba
acul96 <syamsulbasyir82@gmail.com>
2026-07-17 12:24:16 +0000

    render video baru

--- src/Root.tsx
+++ src/Root.tsx
@@ -14,8 +14,8 @@ export const RemotionRoot: React.FC = () => {
         component={HelloWorld}
         durationInFrames={150}
         fps={30}
-        width={1920}
-        height={1080}
+        width={3840}
+        height={2160}
         // You can override these props for each render:
         // https://www.remotion.dev/docs/parametrized-rendering
         schema={myCompSchema}
